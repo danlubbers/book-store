66f7034f61011b11b5177a6629e525da0f4878d0
Dan Lubbers <lubbers.dan@gmail.com>
2020-04-30 18:32:16 -0400

    Setting up Login Page

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Header() {
   return (
     <nav>
       <h1> BookShelf </h1>
-      <h4> Search </h4>
+      <h4> <button className='searchBtn'><FontAwesomeIcon icon={faSearch} size='sm'/></button> Search </h4>
     </nav>
   )
 }
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {FormGroup, FormControl, FormLabel} from 'react-bootstrap';
+
 
 export default function Login() {
+
+const [values, setValues] = useState({
+  username: '', 
+  password: ''
+});
+
+const handleValues = e => {
+  setValues({...values, [e.target.name]: e.target.value})
+}
+
+const handleSubmit = e => {
+  e.preventDefault();
+  console.log(values.username)
+}
+
   return (
-    <>
-      <h1>Login Test</h1>
-    </>
+      <form className='formContainer' onSubmit={handleSubmit}>
+
+        <FormGroup className="mb-3 formGroup" controlId='formBasicEmail' >
+          <FormLabel className='formLabel'>Username</FormLabel>
+          <FormControl className='inputText' type="text" placeholder='Username' aria-label="Username" name='username' value={values.username} onChange={handleValues}/> 
+        </FormGroup>
+
+        <FormGroup className="mb-3 formGroup">
+          <FormLabel className='formLabel'>Password</FormLabel>
+          <FormControl className='inputText' type="password" placeholder='Password' aria-label="Password" name='password' value={values.password} onChange={handleValues}/> 
+        </FormGroup>
+        
+        <button type='submit' className='btn btn-secondary'>Login</button>
+      </form>
   )
 }
