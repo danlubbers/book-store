ff83fd216358f4f7c0ed02ecc8c5d9f179e2e5bb
Dan Lubbers <lubbers.dan@gmail.com>
2020-04-30 00:13:00 -0400

    First Commit with server setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.scss";
+import Header from './Components/Header/Header';
+import Login from './Components/Login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Header() {
+  return (
+    <nav>
+      <h1> BookShelf </h1>
+      <h4> Search </h4>
+    </nav>
+  )
+}
