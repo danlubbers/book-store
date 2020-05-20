1e68ba665cd3c530040926341c4644e22fc78ceb
Dan Lubbers <lubbers.dan@gmail.com>
2020-05-20 14:35:13 -0400

    more testing

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 import { CookieProvider } from './context/cookieContext';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history'; 
+import { act } from 'react-dom/test-utils';
 
 const history = createBrowserHistory();
 
@@ -16,3 +17,37 @@ it('renders without crashing', () => {
     </Router>
   )
 })
+
+// it('submits username and password',  () => {
+//   const username = 'alex';
+//   const password = 'grey';
+//   const onSubmit = jest.fn();
+//   const wrapper = mount(
+//     <Router history={history}>
+//       <CookieProvider>
+//         <App onSubmit={onSubmit}/>
+//       </CookieProvider>
+//     </Router>
+//   )
+
+//   act(() => {
+//     wrapper
+//       .find({'aria-label': 'Username'}).at(0)
+//       .props().onChange({Username: {target: {value: username}}});
+
+//     // wrapper
+//     //   .find({'aria-label': 'Password'}).at(1)
+//     //   .props().onChange({Password: {target: {value: 'password'}}});
+
+//       wrapper.update();
+//       wrapper.find({ 'aria-label': 'login-form' }).props().onSubmit({
+//         preventDefault: () => {}
+//       });
+
+//       expect(onSubmit).toHaveBeenCalledTimes(1);
+//       expect(onSubmit).toHaveBeenCalledWith({
+//         username,
+//         // password
+//       });
+//   })
+// })
--- src/Components/Login/Login.test.js
+++ src/Components/Login/Login.test.js
@@ -1,12 +1,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Login from './Login';
-// import { Router } from 'react-router-dom';
-// import { CookieProvider } from '../../context/cookieContext';
-import { createBrowserHistory } from 'history'; 
-
-// const history = createBrowserHistory();
 
 // Using react dom testing - This test takes 21ms whereas Emzyme takes 2ms.
 // it('renders without crashing', () => {
@@ -33,35 +28,3 @@ it('should render one two buttons', () => {
   const wrapper = shallow(<Login />);
   expect(wrapper.find('button')).toHaveLength(2);
 });
-
-// it('submits username and password',  () => {
-//   const username = 'alex';
-//   const password = 'grey';
-//   const onSubmit = jest.fn();
-//   const wrapper = mount(
-//     <Router history={history}>
-//       <CookieProvider>
-//         <Login onSubmit={onSubmit}/>
-//       </CookieProvider>
-//     </Router>
-//   )
-
-//   wrapper
-//     .find({'data-testid': 'Username'}).at(0)
-//     .simulate('change', {target: {value: username}});
-
-//   wrapper
-//     .find({'data-testid': 'Password'}).at(1)
-//     .simulate('change', {target: {value: password}});
-
-//     wrapper.update();
-//     wrapper.find({ 'data-testid': 'login-form' }).simulate('submit', {
-//       preventDefault: () => {}
-//     });
-
-//     expect(onSubmit).toHaveBeenCalledTimes(1);
-//     expect(onSubmit).toHaveBeenCalledWith({
-//       username,
-//       password
-//     });
-// })
